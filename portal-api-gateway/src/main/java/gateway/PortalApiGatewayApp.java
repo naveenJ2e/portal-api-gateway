bdc5f1ad2b99d2d4ea6b72bdfe9cc278d958cbff
naveenj2e <naveenj2e08@gmail.com>
2023-04-29 17:00:23 +0530

    configure pom file

---
+++ portal-api-gateway/src/main/java/gateway/PortalApiGatewayApp.java
@@ -0,0 +1,14 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class PortalApiGatewayApp {
+    public static void main(String[]args){
+        SpringApplication.run(PortalApiGatewayApp.class, args);
+    }
+
+}
